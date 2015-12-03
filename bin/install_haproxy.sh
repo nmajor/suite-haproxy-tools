16c8374dbf1c84a1203437c41c55eeaea2f932f2
Nicholas Major <nick@nmajor.com>
2015-12-03 16:54:08 +0700

    haproxy install script tweak

--- bin/install_haproxy.sh
+++ bin/install_haproxy.sh
@@ -2,9 +2,14 @@
 
 # install haproxy
 
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+cd "$PARENT_PATH"
+
 apt-get update
 apt-get -y install haproxy
 
 rm -rf /etc/init.d/haproxy
 cp ../script/haproxy.sh /etc/init.d/haproxy
 chmod +x /etc/init.d/haproxy
+
+cd "$PARENT_PATH/.."
