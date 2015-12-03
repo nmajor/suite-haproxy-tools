99aec65a117ba3a269a5a0726f0bdb0ae25144b0
Nicholas Major <nick@nmajor.com>
2015-12-03 16:46:17 +0700

    Updating install ruby script

--- bin/install_ruby.sh
+++ bin/install_ruby.sh
@@ -2,15 +2,9 @@
 
 # Install Ruby
 
-apt-get -y update
-apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
-cd /tmp
-curl -O http://cache.ruby-lang.org/pub/ruby/ruby-2.2.3.tar.gz
-tar -xvzf ruby-2.2.3.tar.gz
-cd /tmp/ruby-2.2.3
-./configure --prefix=/usr/local
-make
-make install
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get -y install ruby2.2 ruby2.2-dev
 
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$PARENT_PATH/.."
