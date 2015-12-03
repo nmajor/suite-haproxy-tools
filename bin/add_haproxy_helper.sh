2024e36d0be1723593c1cf534dffeb0a6e1bdc2a
Nicholas Major <nick@nmajor.com>
2015-12-03 16:52:50 +0700

    Adding init script for haproxy

--- bin/add_haproxy_helper.sh
+++ bin/add_haproxy_helper.sh
@@ -5,5 +5,6 @@ cd "$PARENT_PATH"
 
 rm -rf /sbin/haproxy_helper
 cp ../script/haproxy_helper.rb /sbin/haproxy_helper
+chmod +x /sbin/haproxy_helper
 
 cd "$PARENT_PATH/.."
--- bin/install_haproxy.sh
+++ bin/install_haproxy.sh
@@ -4,3 +4,7 @@
 
 apt-get update
 apt-get -y install haproxy
+
+rm -rf /etc/init.d/haproxy
+cp ../script/haproxy.sh /etc/init.d/haproxy
+chmod +x /etc/init.d/haproxy
