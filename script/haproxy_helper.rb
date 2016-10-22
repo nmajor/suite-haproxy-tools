df01ee9746bba2daf118c8a202832df3bfe24fc7
Nicholas Major <nick@nmajor.com>
2016-10-22 15:54:47 -0600

    Fixing the health check for ssl 2

--- script/haproxy_helper.rb
+++ script/haproxy_helper.rb
@@ -364,7 +364,6 @@ backend #{backend_name(service)}_https
 \ttcp-response content accept if serverhello
 \tstick on payload_lv(43,1) if clienthello
 \tstick store-response payload_lv(43,1) if serverhello
-\toption ssl-hello-chk HEAD /health
 #{ service.healthy_nodes.map{|n| server_text_https(n) }.join }
 EOT
   end
